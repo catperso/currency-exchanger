6e92e267365b00fa77c14eae41c01e53f5d1cc90
Matt Casperson <mc.casperson@gmail.com>
2021-11-19 09:02:49 -0800

    add exchange-service.js to handle the API call

---
+++ src/js/exchange-service.js
@@ -0,0 +1,14 @@
+export default class ExchangeService {  
+  static getExchanged(cashFrom, cashTo, amount) {
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${cashFrom}/${cashTo}/${amount}`)
+      .then(function(response) {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        return response.json();
+      })
+      .catch(function(error) {
+        return Error(error);
+      });
+  }
+}
